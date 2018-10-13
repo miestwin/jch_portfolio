2dabd50fe98f1200366a28bf22d529bb2d64f01e
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2018-10-13 14:40:58 +0200

    Add email service

--- controllers/about.js
+++ controllers/about.js
@@ -5,7 +5,7 @@ const router = express.Router();
  * GET about page
  */
 router.get('/', function (req, res, next) {
-    res.render('about',  { about: true })
+    res.render('about',  { about: true });
 });
 
 module.exports = router;
--- controllers/contact.js
+++ controllers/contact.js
@@ -1,11 +1,42 @@
+const nodemailer = require('nodemailer');
 const express = require('express');
 const router = express.Router();
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    sercure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
 /**
  * GET contact page
  */
 router.get('/', function (req, res, next) {
-    res.render('contact',  { contact: true })
+    res.render('contact',  { contact: true });
+});
+
+/**
+ * Send email
+ */
+router.post('/', function (req, res, next) {
+    const mailOptions = {
+        from: `"${req.body.name}"<${req.body.email}>`,
+        to: process.env.EMAIL_USER, // 'jakub.chelstowski12@wp.pl'
+        subject: 'Wiadomość z portfolio',
+        text: req.body.message
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            res.render('contact',  { contact: true, error: true });
+        } else {
+            res.render('contact',  { contact: true, success: true });
+        }
+    });
 });
 
 module.exports = router;
