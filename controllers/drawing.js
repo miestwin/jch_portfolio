ba0d8d5928ad883defbcf4e0e60cc154d7c670b5
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2018-10-15 20:01:12 +0200

    Code refactoring

--- controllers/drawing.js
+++ controllers/drawing.js
@@ -8,4 +8,8 @@ router.get('/', function (req, res ,next) {
     res.render('index', { drawing: true });
 });
 
+router.get('/gallery/slides', function (req, res, next) {
+    res.render('index', {});
+});
+
 module.exports = router;
